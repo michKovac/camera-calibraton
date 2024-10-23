09763cb21982e2ddce90a682cfae253ce38788c1
m.Kovac <michal.kovac1@stuba.sk>
2024-10-23 02:53:55 +0200

    Refactor image file paths in calibration examples and alignment script

--- alin_examp.py
+++ alin_examp.py
@@ -2,12 +2,12 @@ from src.pair_cameras_calibration.image_aligement import ImageAlignment
 
 swir_path = '/home/michal/Documents/datacollections/1019/undistordted_swir/data2'
 rgb_path = '/home/michal/Documents/datacollections/1019/undistordted_rgb/data2'
-swir_alin_output_path = '/home/michal/Documents/datacollections/1019/aligned_swir/data2'
-rgb_alin_output_path = '/home/michal/Documents/datacollections/1019/aligned_rgb/data2'
+out_path = '/home/michal/Documents/datacollections/1019/aligned/data2'
+
 
 # Call the function with the provided paths
 matrix = 'homography_1019_000786.pkl'
 #matrix = 'homography_matrix.pkl'
 alingment = ImageAlignment(matrix)
-alingment.align_batch(swir_path, rgb_path, swir_alin_output_path, rgb_alin_output_path, show=True)
+alingment.align_batch(swir_path, rgb_path, out_path, show=True)
 
