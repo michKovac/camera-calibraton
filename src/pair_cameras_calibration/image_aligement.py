fe1d5c2e3285f81f2268e8e27a6549f9fcaaaaff
m.Kovac <michal.kovac1@stuba.sk>
2025-02-20 22:42:17 +0100

    refactor aligning

--- src/pair_cameras_calibration/image_aligement.py
+++ src/pair_cameras_calibration/image_aligement.py
@@ -7,10 +7,10 @@ import tqdm
 class ImageAlignment:
     def __init__(self, homography_matrix=None, homography_matrix_nir=None):
         """
-        Initialize with optional homography matrices for RGB-SWIR and NIR-SWIR alignment.
+        Initialize with optional homography matrices for SWIR-RGB and NIR-RGB alignment.
         
-        :param homography_matrix: Optional precomputed RGB-SWIR homography matrix
-        :param homography_matrix_nir: Optional precomputed NIR-SWIR homography matrix
+        :param homography_matrix: Optional precomputed SWIR-RGB homography matrix
+        :param homography_matrix_nir: Optional precomputed NIR-RGB homography matrix
         """
         self.descriptor = cv.SIFT.create()
         self.matcher = cv.FlannBasedMatcher()
@@ -57,24 +57,24 @@ class ImageAlignment:
 
     def calculate_homography(self, image_path_rgb, image_path_swir, image_path_nir=None, lowe_ratio=0.75):
         """
-        Calculate homography matrices for RGB-SWIR and optionally NIR-SWIR alignment.
+        Calculate homography matrices for SWIR-RGB and optionally NIR-RGB alignment.
         
         :param image_path_rgb: Path to RGB image
         :param image_path_swir: Path to SWIR image
         :param image_path_nir: Optional path to NIR image
-        :return: Tuple of homography matrices (rgb_swir_matrix, nir_swir_matrix)
+        :return: Tuple of homography matrices (rgb-swir, nir_rgb_matrix)
         """
         # Calculate RGB-SWIR homography
         swir_rgb_matrix = self._calculate_single_homography(image_path_rgb, image_path_swir, lowe_ratio)
         self.homography_matrix = swir_rgb_matrix
-        self.__save('homography_matrix_rgb.pkl')
+        self.__save('homography_matrix_swir_rgb.pkl')
 
         # Calculate NIR-SWIR homography if NIR image provided
         nir_rgb_matrix = None
         if image_path_nir:
             nir_rgb_matrix = self._calculate_single_homography(image_path_nir, image_path_swir, lowe_ratio)
             self.homography_matrix_nir = nir_rgb_matrix
-            self.__save('homography_matrix_nir.pkl', is_nir=True)
+            self.__save('homography_matrix_nir_rgb.pkl', is_nir=True)
 
         return swir_rgb_matrix, nir_rgb_matrix
 
@@ -131,35 +131,35 @@ class ImageAlignment:
         self.__save('chess_homography_matrix.pkl')
         return self.homography_matrix
     
-    def align_images(self, path_swir, path_rgb, path_nir=None, homography_mat_rgb=None, homography_mat_nir=None):
+    def align_images(self, path_swir, path_rgb, path_nir=None, homography_mat_swir_rgb=None, homography_mat_nir_rgb=None):
         """
         Align RGB and optionally NIR images to SWIR image.
         
         :return: Tuple of (aligned_rgb, aligned_nir, original_swir) or (aligned_rgb, original_swir)
         """
-        if homography_mat_rgb is None:
-            homography_mat_rgb = self.homography_matrix
+        if homography_mat_swir_rgb is None:
+            homography_mat_swir_rgb = self.homography_matrix
 
         # Load NIR homography matrix if not provided
-        if homography_mat_nir is None:
-            homography_mat_nir = self.homography_matrix_nir
+        if homography_mat_nir_rgb is None:
+            homography_mat_nir_rgb = self.homography_matrix_nir
 
         # Read the images
         swir_img = cv.imread(path_swir, cv.IMREAD_COLOR)
         rgb_img = cv.imread(path_rgb, cv.IMREAD_COLOR)
 
         # Warp the SWIR image to RGB perspective
-        warped_swir = cv.warpPerspective(swir_img, homography_mat_rgb, (rgb_img.shape[1], rgb_img.shape[0]))
+        warped_swir = cv.warpPerspective(swir_img, homography_mat_swir_rgb, (rgb_img.shape[1], rgb_img.shape[0]))
         # Convert to BGR if necessary (assuming SWIR is in grayscale)
         warped_swir = cv.cvtColor(warped_swir, cv.COLOR_BGR2GRAY)
         warped_swir = cv.cvtColor(warped_swir, cv.COLOR_GRAY2BGR)
 
-        if path_nir and homography_mat_nir is not None:
+        if path_nir and homography_mat_nir_rgb is not None:
             nir_img = cv.imread(path_nir, cv.IMREAD_COLOR)
             # Warp the NIR image to RGB perspective
             nir_img = cv.imread(path_nir, cv.IMREAD_COLOR)
             nir_img = cv.resize(nir_img, (rgb_img.shape[1], rgb_img.shape[0]))
-            warped_nir = cv.warpPerspective(nir_img, homography_mat_nir, (rgb_img.shape[1], rgb_img.shape[0]), flags=cv.INTER_LINEAR)
+            warped_nir = cv.warpPerspective(nir_img, homography_mat_nir_rgb, (rgb_img.shape[1], rgb_img.shape[0]), flags=cv.INTER_LINEAR)
             # Convert to BGR if necessary
             warped_nir = cv.cvtColor(warped_nir, cv.COLOR_BGR2GRAY)
             warped_nir = cv.cvtColor(warped_nir, cv.COLOR_GRAY2BGR)
@@ -222,15 +222,27 @@ class ImageAlignment:
             rgb[top:bottom, left:right]
         )
         
-    def update_opacity(self,x, rgb_alin, swir_alin):
-        alpha = x / 100
-        beta = 1 - alpha
-        # Resize the images by half
-        rgb_alin_resized = cv.resize(rgb_alin, (rgb_alin.shape[1] // 1, rgb_alin.shape[0] // 1))
-        swir_alin_resized = cv.resize(swir_alin, (swir_alin.shape[1] //1, swir_alin.shape[0] // 1))
+    def update_opacity(self, rgb_alpha, swir_alpha, nir_alpha, rgb_alin, swir_alin, nir_alin):
+        # Resize the images to the same dimensions
+        height = max(rgb_alin.shape[0], swir_alin.shape[0], nir_alin.shape[0])
+        width = max(rgb_alin.shape[1], swir_alin.shape[1], nir_alin.shape[1])
+        
+        rgb_alin_resized = cv.resize(rgb_alin, (width, height))
+        swir_alin_resized = cv.resize(swir_alin, (width, height))
+        nir_alin_resized = cv.resize(nir_alin, (width, height))
+        
+        # Normalize alpha values to be between 0 and 1
+        rgb_alpha = rgb_alpha / 100.0
+        swir_alpha = swir_alpha / 100.0
+        nir_alpha = nir_alpha / 100.0
         
         # Blend the resized images
-        blended = cv.addWeighted(rgb_alin_resized, alpha, swir_alin_resized, beta, 0)
+        blended = cv.addWeighted(rgb_alin_resized, rgb_alpha, swir_alin_resized, swir_alpha, 0)
+        blended = cv.addWeighted(blended, 1.0, nir_alin_resized, nir_alpha, 0)  # Blend NIR image
+        
+        # Ensure the blended image is in the correct format
+        blended = np.clip(blended, 0, 255).astype(np.uint8)
+        
         cv.imshow('Blended', blended)
         
     def align_batch(self, swir_path, rgb_path, nir_path=None, output_path="aligned", homography_mat=None, show=False):
@@ -259,7 +271,14 @@ class ImageAlignment:
 
         if show:
             cv.namedWindow('Blended')
-            cv.createTrackbar('Opacity', 'Blended', 0, 100, lambda x: self.update_opacity(x, rgb_alin, swir_alin))
+            cv.createTrackbar('RGB Opacity', 'Blended', 100, 100, lambda x: self.update_opacity(x / 100, swir_alpha / 100, nir_alpha / 100, rgb_alin, swir_alin, nir_alin))
+            cv.createTrackbar('SWIR Opacity', 'Blended', 100, 100, lambda x: self.update_opacity(rgb_alpha / 100, x / 100, nir_alpha / 100, rgb_alin, swir_alin, nir_alin))
+            cv.createTrackbar('NIR Opacity', 'Blended', 100, 100, lambda x: self.update_opacity(rgb_alpha / 100, swir_alpha / 100, x / 100, rgb_alin, swir_alin, nir_alin))
+
+        # Initialize opacity values
+        rgb_alpha = 100
+        swir_alpha = 100
+        nir_alpha = 100
 
         index = 0
         print('Starting image alignment...')
@@ -291,9 +310,12 @@ class ImageAlignment:
 
             pbar.update(1)
 
+            # Update opacity with NIR image
             if show:
-                opac = cv.getTrackbarPos('Opacity', 'Blended') 
-                self.update_opacity(opac, rgb_alin, swir_alin)  # Initialize with the first call
+                opac_rgb = cv.getTrackbarPos('RGB Opacity', 'Blended') 
+                opac_swir = cv.getTrackbarPos('SWIR Opacity', 'Blended') 
+                opac_nir = cv.getTrackbarPos('NIR Opacity', 'Blended') 
+                self.update_opacity(opac_rgb, opac_swir, opac_nir, rgb_alin, swir_alin, nir_alin)  # Initialize with the first call
 
                 while True:
                     key = cv.waitKey(1) & 0xFF
@@ -306,8 +328,10 @@ class ImageAlignment:
                     elif key == ord('q'):  # 'q' key to quit
                         cv.destroyAllWindows()
                         exit()
-                    opac = cv.getTrackbarPos('Opacity', 'Blended')
-                    self.update_opacity(opac, rgb_alin, swir_alin)  # Update opacity on trackbar change
+                    opac_rgb = cv.getTrackbarPos('RGB Opacity', 'Blended')
+                    opac_swir = cv.getTrackbarPos('SWIR Opacity', 'Blended')
+                    opac_nir = cv.getTrackbarPos('NIR Opacity', 'Blended')
+                    self.update_opacity(opac_rgb, opac_swir, opac_nir, rgb_alin, swir_alin, nir_alin)  # Update opacity on trackbar change
             else:
                 index += 1
                 if index >= len(swir_images):
